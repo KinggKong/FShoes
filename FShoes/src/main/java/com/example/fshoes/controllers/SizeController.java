c990ce379c391c97463921dd9ada00bf05d390c3
letrunghieu07 <trunghieu07032003@gamil.com>
2024-07-03 18:02:07 +0700

    update bán hàng load SPCT

--- FShoes/src/main/java/com/example/fshoes/controllers/SizeController.java
+++ FShoes/src/main/java/com/example/fshoes/controllers/SizeController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -67,18 +68,37 @@ public class SizeController {
         return "redirect:/sizes/list";
     }
 
+//    @PostMapping("/update")
+//    public String UpdateSize(@ModelAttribute("size") Size size, RedirectAttributes redirectAttributes) {
+//        sizeService.updateSize(size);
+//        redirectAttributes.addFlashAttribute("message2", "Sửa dữ liệu thành công!");
+//        return "redirect:/sizes/list";
+//    }
+//
+//    @GetMapping("/detail/{id}")
+//    public String getSizeDetail(@PathVariable("id") Long sizeID, Model model) {
+//        Size size = sizeService.findSizeById(sizeID);
+//        model.addAttribute("size", size);
+//        return "home/size";
+//    }
+
     @PostMapping("/update")
-    public String UpdateSize(@ModelAttribute("size") Size size, RedirectAttributes redirectAttributes) {
-        sizeService.updateSize(size);
-        redirectAttributes.addFlashAttribute("message2", "Sửa dữ liệu thành công!");
+    public String UpdateSize(@RequestParam("sizeId") Long sizeId,
+                             @RequestParam("sizeName") String sizeName) {
+        Size updateSize = new Size();
+        updateSize.setId(sizeId);
+        updateSize.setName(sizeName);
+        sizeService.updateSize(updateSize);
         return "redirect:/sizes/list";
     }
 
     @GetMapping("/detail/{id}")
-    public String getSizeDetail(@PathVariable("id") Long sizeID, Model model) {
-        Size size = sizeService.findSizeById(sizeID);
-        model.addAttribute("size", size);
-        return "home/size";
+    @ResponseBody
+    public ResponseEntity<Size> getSizeDetail(@PathVariable("id") Long colorId, Model model) {
+        Size color = sizeService.findSizeById(colorId);
+        model.addAttribute("size", sizeService.findSizeById(colorId));
+        model.addAttribute("message2", "Sửa dữ liệu thành công");
+        return ResponseEntity.ok(color);
     }
 
     @GetMapping("delete/{id}")
