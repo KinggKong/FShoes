f2c5a3d29e57682a5111475582a0826b97531b94
thonghtph43253 <hoangthong2105@gmail.com>
2024-07-04 13:45:10 +0700

    up entity

--- FShoes/src/main/java/com/example/fshoes/entities/Brand.java
+++ FShoes/src/main/java/com/example/fshoes/entities/Brand.java
@@ -1,2 +1,25 @@
-package com.example.fshoes.entities;public class Brand {
+package com.example.fshoes.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "brand")
+public class Brand extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "brand_name", nullable = false, length = 50)
+    private String brandName;
+
+
+    @Column(name = "status")
+    private int status;
 }
--- FShoes/src/main/java/com/example/fshoes/repositories/DanhMucRepository.java
+++ FShoes/src/main/java/com/example/fshoes/repositories/DanhMucRepository.java
@@ -2,6 +2,8 @@ package com.example.fshoes.repositories;
 
 import com.example.fshoes.entities.DanhMuc;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DanhMucRepository extends JpaRepository<DanhMuc, Long> {
 }
